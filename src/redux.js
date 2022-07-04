9a9d639bd7630e00b4db6106325db70115d7cf60
jjunghoo <wjdgn2468@naver.com>
2022-07-05 04:01:08 +0900

    fix: input 상태관리를 위해 redux 사용

---
+++ src/redux.js
@@ -0,0 +1,26 @@
+const initialFormData = {
+    id: '',
+    pw: '',
+    confirmPw: '',
+}
+
+export const setInputValue = (id, value) => {
+    // console.log(id, value)
+    return {
+        type: id,
+        payload: value,
+    }
+}
+
+export const reducer = (state = initialFormData, action) => {
+    switch (action.type) {
+        case 'id':
+            return { ...state, id: action.payload }
+        case 'pw':
+            return { ...state, pw: action.payload }
+        case 'confirmPw':
+            return { ...state, confirmPw: action.payload }
+        default:
+            return state
+    }
+}
